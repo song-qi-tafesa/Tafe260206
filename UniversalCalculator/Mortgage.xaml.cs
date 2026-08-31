9abb57bc531a7fdcae27abc45997658aeb445dfc
Song Qi <songmadd@gmail.com>
2026-08-31 13:08:41 +0930

    Finished main menu

--- UniversalCalculator/Mortgage.xaml.cs
+++ UniversalCalculator/Mortgage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -21,34 +20,11 @@ namespace Calculator
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class Menu : Page
+	public sealed partial class Mortgage : Page
 	{
-		public Menu()
+		public Mortgage()
 		{
 			this.InitializeComponent();
 		}
-
-		private void Math_Button_Click(object sender, RoutedEventArgs e)
-
-		{
-			this.Frame.Navigate(typeof(MainPage));
-		}
-
-		private void Mortgage_Button_Click(object sender, RoutedEventArgs e)
-		{
-			this.Frame.Navigate(typeof(Mortgage));
-		}
-
-		private void Currency_Button_Click(object sender, RoutedEventArgs e)
-		{
-			this.Frame.Navigate(typeof(Currency));
-		}
-
-		private void Exit_Button_Click(object sender, RoutedEventArgs e)
-		{
-			CoreApplication.Exit();
-		}
 	}
-
-	
 }
