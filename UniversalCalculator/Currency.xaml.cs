79fc253397e17a575f98c48522ad9f5ca76910be
Ryan Thomas <ryan.thomas2@student.tafesa.edu.au>
2026-09-02 05:28:20 +0930

    Added pseudocode for currency conversion functionality

--- UniversalCalculator/Currency.xaml.cs
+++ UniversalCalculator/Currency.xaml.cs
@@ -26,5 +26,73 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
-	}
+		//function for presenting information
+		// private void conversionButton_Click(object sender, RoutedEventArgs e):
+
+		// decimal amount
+		// decimal result
+		// amount = amountInput
+		// result = conversionFunction(amount, selection1, selection2)
+
+		// initialRequestText = amount + selection1 =
+		// calculationText = result + selection2
+		// amount = 1
+		// baseConversionOneText = (1 selection1) = conversionFunction(amount, selection1, selection2) selection2
+		// baseConversionTwoText = (1 selection2) = conversionFunction(amount, selection2, selection1) selection1
+
+		//function for conversion
+		// private conversionFunction(amount)
+
+		// decimal conversionRate(amount, selection1, selection2)
+
+		// if selection1 = US
+		//		if selection2 = Euro:
+		//			conversionRate = 0.85189982
+		//			result = amount x conversionRate
+		//		if selection2 = British Pound:
+		//			conversionRate = 0.72872436
+		//			result = amount x conversionRate
+		//		if selection2 = Indian Rupee:
+		//			conversionRate = 74.257327
+		//			result = amount x conversionRate
+
+		// if selection1 = Euro
+		//		if selection2 = US:
+		//			conversionRate = 1.1739732
+		//			result = amount x conversionRate
+		//		if selection2 = British Pound:
+		//			conversionRate = 0.8556672
+		//			result = amount x conversionRate
+		//		if selection2 = Indian Rupee:
+		//			conversionRate = 87.00755
+		//			result = amount x conversionRate
+
+		// if selection1 = British Pound
+		//		if selection2 = Euro:
+		//			conversionRate = 1.1686692
+		//			result = amount x conversionRate
+		//		if selection2 = US:
+		//			conversionRate = 1.371907
+		//			result = amount x conversionRate
+		//		if selection2 = Indian Rupee:
+		//			conversionRate = 101.68635
+		//			result = amount x conversionRate
+
+		// if selection1 = Indian Rupee
+		//		if selection2 = Euro:
+		//			conversionRate = 0.013492774
+		//			result = amount x conversionRate
+		//		if selection2 = British Pound:
+		//			conversionRate = 0.0098339397
+		//			result = amount x conversionRate
+		//		if selection2 = US:
+		//			conversionRate = 0.011492628
+		//			result = amount x conversionRate
+
+		// private void Exit_Button_Click(object sender, RoutedEventArgs e)
+		//{
+			//CoreApplication.Exit();
+		//}
+
+}
 }
