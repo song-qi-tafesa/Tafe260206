eb65e5f4cb066fc03c2c89598ce794ea37c9d317
Ryan Thomas <ryan.thomas2@student.tafesa.edu.au>
2026-09-03 04:02:16 +0930

    Latest version of Currency Conversion Calculator, functional and commented

--- UniversalCalculator/Currency.xaml.cs
+++ UniversalCalculator/Currency.xaml.cs
@@ -26,73 +26,171 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
-		//function for presenting information
-		// private void conversionButton_Click(object sender, RoutedEventArgs e):
+		//main function for presenting the calculations when button clicked
+		private void Calculate_Currency_Button_Click(object sender, RoutedEventArgs e)
+		{
+			//set up variables
+			double amount;
+			double result;
+			string fromCurrency ="e";
+			string toCurrency ="e";
+			amount = double.Parse(amountTextBox.Text);
 
-		// decimal amount
-		// decimal result
-		// amount = amountInput
-		// result = conversionFunction(amount, selection1, selection2)
+			//grab the tag for each of the combo boxes
+			var item1 = fromBox.SelectedItem as ComboBoxItem;
+			var item2 = toBox.SelectedItem as ComboBoxItem;
 
-		// initialRequestText = amount + selection1 =
-		// calculationText = result + selection2
-		// amount = 1
-		// baseConversionOneText = (1 selection1) = conversionFunction(amount, selection1, selection2) selection2
-		// baseConversionTwoText = (1 selection2) = conversionFunction(amount, selection2, selection1) selection1
+			string fromBoxTag = item1?.Tag?.ToString();
+			string toBoxTag = item2?.Tag?.ToString();
 
-		//function for conversion
-		// private conversionFunction(amount)
+			//define the currencies for presenting in text
+			if (fromBoxTag == "fromUS")
+			{
+				fromCurrency = "USD";
+			}
+			if (fromBoxTag == "fromEuro")
+			{
+				fromCurrency = "EUR";
+			}
+			if (fromBoxTag == "fromBritish")
+			{
+				fromCurrency = "GBP";
+			}
+			if (fromBoxTag == "fromIndian")
+			{
+				fromCurrency = "INR";
+			}
+			if (toBoxTag == "toUS")
+			{
+				toCurrency = "USD";
+			}
+			if (toBoxTag == "toEuro")
+			{
+				toCurrency = "EUR";
+			}
+			if (toBoxTag == "toBritish")
+			{
+				toCurrency = "GBP";
+			}
+			if (toBoxTag == "toIndian")
+			{
+				toCurrency = "INR";
+			}
 
-		// decimal conversionRate(amount, selection1, selection2)
+			//calculate conversion rate
+			result = ConversionFunction(amount, fromBoxTag, toBoxTag);
 
-		// if selection1 = US
-		//		if selection2 = Euro:
-		//			conversionRate = 0.85189982
-		//			result = amount x conversionRate
-		//		if selection2 = British Pound:
-		//			conversionRate = 0.72872436
-		//			result = amount x conversionRate
-		//		if selection2 = Indian Rupee:
-		//			conversionRate = 74.257327
-		//			result = amount x conversionRate
+			//present the conversion 
+			initialRequestText.Text = Math.Round(amount, 2).ToString() + " " + fromCurrency + " =";
+			calculationText.Text = Math.Round(result, 2).ToString() + " " + toCurrency;
 
-		// if selection1 = Euro
-		//		if selection2 = US:
-		//			conversionRate = 1.1739732
-		//			result = amount x conversionRate
-		//		if selection2 = British Pound:
-		//			conversionRate = 0.8556672
-		//			result = amount x conversionRate
-		//		if selection2 = Indian Rupee:
-		//			conversionRate = 87.00755
-		//			result = amount x conversionRate
+			//give example of base conversion rate
+			amount = 1;
+			baseConversionOneText.Text = "1 " + fromCurrency + " = " + ConversionFunction(amount, fromBoxTag, toBoxTag) + " " + toCurrency;
+			baseConversionTwoText.Text = "1 " + toCurrency + " = " + ConversionFunction(amount, toBoxTag, fromBoxTag) + " " + fromCurrency;
+		}
+		//function for conversion rate
+		private double ConversionFunction(double amount, string selection1, string selection2)
+		{
+			//set variables
+			double conversionRate;
+			double result;
 
-		// if selection1 = British Pound
-		//		if selection2 = Euro:
-		//			conversionRate = 1.1686692
-		//			result = amount x conversionRate
-		//		if selection2 = US:
-		//			conversionRate = 1.371907
-		//			result = amount x conversionRate
-		//		if selection2 = Indian Rupee:
-		//			conversionRate = 101.68635
-		//			result = amount x conversionRate
+			//decide which conversion rate needs to be used, and calculate the result
+			if (selection1 == "fromUS" || selection1 == "toUS")
+			{
+				if (selection2 == "toEuro" || selection2 == "fromEuro")
+				{
+					conversionRate = 0.85189982;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toBritish" || selection2 == "fromBritish")
+				{
+					conversionRate = 0.72872436;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toIndian" || selection2 == "fromIndian")
+				{
+					conversionRate = 74.257327;
+					result = amount * conversionRate;
+					return result;
+				}
+			}
+			if (selection1 == "fromEuro" || selection1 == "toEuro")
+			{
+				if (selection2 == "toUS" || selection2 == "fromUS")
+				{
+					conversionRate = 1.1739732;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toBritish" || selection2 == "fromBritish")
+				{
+					conversionRate = 0.8556672;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toIndian" || selection2 == "fromIndian")
+				{
+					conversionRate = 87.00755;
+					result = amount * conversionRate;
+					return result;
+				}
+			}
+			if (selection1 == "fromBritish" || selection1 == "toBritish")
+			{
+				if (selection2 == "toEuro" || selection2 == "fromEuro")
+				{
+					conversionRate = 1.1686692;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toUS" || selection2 == "fromUS")
+				{
+					conversionRate = 1.371907;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toIndian" || selection2 == "fromIndian")
+				{
+					conversionRate = 101.68635;
+					result = amount * conversionRate;
+					return result;
+				}
+			}
+			if (selection1 == "fromIndian" || selection1 == "toIndian")
+			{
+				if (selection2 == "toEuro" || selection2 == "fromEuro")
+				{
+					conversionRate = 0.013492774;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toBritish" || selection2 == "fromBritish")
+				{
+					conversionRate = 0.0098339397;
+					result = amount * conversionRate;
+					return result;
+				}
+				if (selection2 == "toUS" || selection2 == "fromUS")
+				{
+					conversionRate = 0.011492628;
+					result = amount * conversionRate;
+					return result;
+				}
+			}
 
-		// if selection1 = Indian Rupee
-		//		if selection2 = Euro:
-		//			conversionRate = 0.013492774
-		//			result = amount x conversionRate
-		//		if selection2 = British Pound:
-		//			conversionRate = 0.0098339397
-		//			result = amount x conversionRate
-		//		if selection2 = US:
-		//			conversionRate = 0.011492628
-		//			result = amount x conversionRate
+			//should only return 3.6 if there is an error finding the conversion rate
+			return 3.6;
+		
+		}
+		//quit the program
+		private void Exit_Button_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Exit();
+		}
 
-		// private void Exit_Button_Click(object sender, RoutedEventArgs e)
-		//{
-			//CoreApplication.Exit();
-		//}
-
-}
+	}
 }
