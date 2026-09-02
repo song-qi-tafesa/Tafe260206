d187fc18dc7035d5c87dcc52be48020888017c50
RyanT2Tafe <ryan.thomas2@student.tafesa.edu.au>
2026-09-03 04:25:15 +0930

    Revert "Currency Conversion Merge"

--- UniversalCalculator/App.xaml.cs
+++ UniversalCalculator/App.xaml.cs
@@ -30,7 +30,7 @@ namespace Calculator
 			if (e.PrelaunchActivated == false)
 			{
 				if (rootFrame.Content == null)
-					rootFrame.Navigate(typeof(Menu), e.Arguments);
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
 
 				Window.Current.Activate();
 			}
