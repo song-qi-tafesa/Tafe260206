afd1df096912f35cf032f1a40830f7b12aecbd58
Song Qi <songmadd@gmail.com>
2026-09-02 14:15:58 +0930

    completed the mortgage calculator coding

--- UniversalCalculator/Mortgage.xaml.cs
+++ UniversalCalculator/Mortgage.xaml.cs
@@ -26,5 +26,43 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void Calculate_Button_Click(object sender, RoutedEventArgs e)
+		{
+			double annualInterestRate = double.Parse(annualInterestRateTextBox.Text);
+			double principal = double.Parse(principalTextBox.Text);
+			int years = int.Parse(yearsTextBox.Text);
+			int months = int.Parse(monthsTextBox.Text);
+
+			double monthlyInterestRate = CalculateMonthlyInterestRate(annualInterestRate);
+			double repayment = CalculateRepayment(annualInterestRate, principal, years, months);
+
+			monthlyInterestRateTextBox.Text = monthlyInterestRate.ToString("N4");
+			monthlyRepaymentTextBox.Text = repayment.ToString("N2");
+		}
+
+		private double CalculateMonthlyInterestRate(double annualInterestRate)
+		{
+			return (annualInterestRate / 12.0) * 0.01;
+		}
+
+
+
+		private double CalculateRepayment(double annualInterestRate, double principal, int years, int months)
+		{
+			double monthlyInterestRate = CalculateMonthlyInterestRate(annualInterestRate);
+			int numberOfPayments = (years * 12) + months;
+
+			double numerator = principal * Math.Pow(1 + monthlyInterestRate, numberOfPayments) * monthlyInterestRate;
+			double denominator = Math.Pow(1 + monthlyInterestRate, numberOfPayments) - 1;
+
+			return numerator / denominator;
+		}
+
+
+		private void Exit_Button_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Exit();
+		}
 	}
 }
